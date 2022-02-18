2fa245073f91068ab795632f137191bf02e0943a
tanupoo <github-shoichi@tanu.org>
2022-02-18 15:01:05 +0900

    renamed

--- GuardExplosion.gd
+++ GuardExplosion.gd
@@ -8,8 +8,12 @@ var a = 3
 func start(pos0, v0=2.2, a=3):
     position = pos0
     scale = Vector2()
+    
+func _ready():
     $Sprite.show()
-
+    $ExplosionFire.emitting = true
+    $ExplosionSmoke.emitting = true
+    
 func _process(delta):
     t += delta
     scale = Vector2(1,1)*get_coeff(t)
@@ -19,5 +23,4 @@ func get_coeff(t):
     if coeff < 0:
         queue_free()
     return coeff
-
 
